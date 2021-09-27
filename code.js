800ff9295192f23bb852a9c2f3884887e59aa360
Junaid Sheikh <81684239+junaid-sheikh10@users.noreply.github.com>
2021-09-27 22:45:02 +0530

    style: styled a bit

--- code.js
+++ code.js
@@ -8,20 +8,27 @@ var txtinput=document.querySelector("#txt");
 var button=document.querySelector("#btn");
 var division=document.querySelector("#divi");
 
-var serverURL="https://api.funtranslations.com/translate/pirate.json?text=Hello%20sir%21%20my%20mother%20goes%20with%20me%20to%20the%20ocean."
+//var serverURL="https://api.funtranslations.com/translate/pirate.json?text=Hello%20sir%21%20my%20mother%20goes%20with%20me%20to%20the%20ocean."
 
+var serverURL="	https://api.funtranslations.com/translate/ferb-latin.json"
 
 function serverHandler(t){
      return serverURL+"?"+"text"+t
 }
 
+function errorHandler(){
+    alert("server is down , please try after some time")
+}
+
 function clickHandler(){
     var input=txtinput.value;
     console.log(input);
-    fetch()
+    fetch(serverHandler(serverURL))
+    .then(Response=>Response.json())
+    .then(json=>{division.innerText=json.contents.translated})
+    .catch(errorHandler)
 
-
-    division.innerText=input;
+    //division.innerText=input;
     
 }
 
